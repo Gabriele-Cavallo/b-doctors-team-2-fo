a97f241a6d182a79b966f908de1df3edebf43084
MatteD04 <matteo.dantonio04@gmail.com>
2024-07-01 15:26:05 +0200

    feat: add style to AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,8 +6,11 @@ export default {
 
 <template>
     <header>
-        <div class="container">
-            <ul class="nav nav-underline">
+        <div class="container d-flex justify-content-around">
+            <div>
+                <h1>BD<i class="fa-solid fa-stethoscope"></i>ctors</h1>
+            </div>
+            <ul class="nav nav-underline align-self-center">
                 <li class="nav-item">
                     <router-link class="nav-link link-dark" aria-current="page" :to="{name: 'home'}">Homepage</router-link>
                 </li>
@@ -21,3 +24,14 @@ export default {
         </div>
     </header>
 </template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+h1{
+    color: $primary-color;
+}
+i{
+    color: $primary-color;
+}
+</style> 
