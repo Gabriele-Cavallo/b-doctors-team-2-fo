5eaa8986a0aecbc2a09ee162c99acf785cdc72f6
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-07-02 17:47:52 +0200

    feat: add AppLoader component

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,18 @@
+<script>
+    export default{
+        name: 'AppLoader',
+    }
+</script>
+
+<template>
+    <div class="loading justify-content-center d-flex align-items-center">Loading...</div>
+</template>
+
+<style scoped lang="scss">
+    @use '../style/partials/variables' as *;
+.loading{
+    height: 100vh;
+    font-weight: bold;
+    font-size: 40px;
+}
+</style>
