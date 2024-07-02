f47ecd4f5456ff3b9c0e484f9edb1781efd96929
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-07-02 16:44:53 +0200

    feat: add SingleDoctor forms and functions

--- src/pages/SingleDoctor.vue
+++ src/pages/SingleDoctor.vue
@@ -9,14 +9,76 @@
                 store,
                 doctor: null,
                 loading: false,
+                submit: false,
+                userName: '',
+                userEmail: '',
+                userMessage: '',
+                userObject: '',
+                errors: {},
+                userNameReview: '',
+                userMessageReview: '',
+                submitReview: false,
+                userTc: '',
+                userTcReview: '',
             }
         },
         methods: {
+            // Funzione che chiama dal db il singolo dottore
             getSingleDoctor() {
                 axios.get(`${this.store.apiUrl}/api/profiles/${this.$route.params.slug}`)
                 .then((response) => {
                     this.doctor = response.data.profile;
                     this.loading = true;
+                })
+            },
+            // Funzione che manda al db il messaggio utente
+            sendMessage() {
+                const userMessage = {
+                    name: this.userName,
+                    email: this.userEmail,
+                    message: this.userMessage,
+                    object: this.userObject,
+                    accepted_tc: this.userTc
+                };
+
+                this.submit = true;
+
+                axios.post(`${this.store.apiUrl}/api/messages`, userMessage)
+                .then(response => {
+                    if(response.data.success) {
+                        this.errors = {},
+                        this.userName = '';
+                        this.userEmail = '';
+                        this.userMessage = '';
+                        this.userObject = '';
+                        this.userTc = '';
+                    } else {
+                        this.errors = response.data.errors;
+                    }
+                    this.submit = false;
+                })
+            },
+            // Funzione che manda al db la recensione dell' utente
+            sendReview() {
+                const userReview = {
+                    name: this.userNameReview,
+                    description: this.userMessageReview,
+                    // accepted_tc: this.userTcReview
+                };
+
+                this.submitReview = true;
+
+                axios.post(`${this.store.apiUrl}/api/reviews`, userReview)
+                .then(response => {
+                    if(response.data.success) {
+                        this.errors = {},
+                        this.userNameReview = '';
+                        this.userMessageReview = '';
+                        // this.userTcReview = '';
+                    } else {
+                        this.errors = response.data.errors;
+                    }
+                    this.submitReview = false;
                 })
             },
         },
@@ -27,7 +89,7 @@
 </script>
 
 <template>
-    <section>
+    <section class="py-4">
         <div class="container py-3" v-if="loading && doctor">
             <div class="wrapper d-flex align-items-center justify-content-between">
                 <h1>{{ doctor.spec_name }}</h1>
@@ -50,6 +112,124 @@
         <div v-else class="container py-3">
             <p>Loading...</p>
         </div>
+
+        <!-- Form invio messaggio al dottore -->
+        <div class="contact container p-2 mb-4">
+            <h2>CONTATTA IL MEDICO</h2>
+            <div class="danger mb-2">* campi obbligatori</div>
+            <form @submit.prevent="sendMessage">
+
+                <!-- Input user name -->
+                <div class="mb-3">
+                    <label class="form-check-label me-2" for="name">* Nome</label>
+                    <input type="text" name="name" id="name" v-model="userName">
+                </div>
+                <div v-if="errors.name">
+                    <div v-for="error in errors.name" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div>
+                <!-- /Input user name -->
+
+                <!-- Input object -->
+                <div class="mb-3">
+                    <label class="form-check-label me-2" for="object">* Oggetto</label>
+                    <input type="text" name="object" id="object" v-model="userObject">
+                </div>
+                <div v-if="errors.name">
+                    <div v-for="error in errors.name" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div>
+                <!-- /Input object -->
+
+                <!-- Input user email -->
+                <div class="mb-3">
+                    <label for="email" class="form-label me-2">* Email</label>
+                    <input type="email" id="email" aria-describedby="emailHelp" name="email" v-model="userEmail">
+                </div>
+                <div v-if="errors.email">
+                    <div v-for="error in errors.email" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div>
+                <!-- /Input user email -->
+
+                <!-- Input user message -->
+                <div class="mb-3">
+                    <label for="message" class="form-label ms-label">* Chiedo al medico:</label>
+                    <textarea name="message" rows="10" id="message" v-model="userMessage"></textarea>
+                </div>
+                <div v-if="errors.message">
+                    <div v-for="error in errors.message" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div>
+                <!-- /Input user message -->
+
+                <!-- Accept TC -->
+                <div class="mb-3 form-check">
+                    <label class="form-check-label" for="accepted_tc">Accetta termini e condizioni <router-link :to="{name: 'terms-and-conditions-messages'}" target="_blank">Mostra termini e condizioni</router-link></label>
+                    <input type="checkbox" class="form-check-input" name="accepted_tc" id="accepted_tc" v-model="userTc">
+                </div>
+                <div v-if="errors.accepted_tc">
+                    <div v-for="error in errors.accepted_tc" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div>
+                <!-- /Accept TC -->
+
+                <button :disable="submit" type="submit" class="btn btn-brand">{{ submit ? 'Invio...' : 'Invia'}}</button>
+            </form>
+        </div>
+        <!-- /Form invio messaggio al dottore -->
+
+        <!-- Form recensione dottore -->
+        <div class="review-wrapper container p-2">
+            <h2>LASCIA UNA RECENSIONE</h2>
+            <div class="danger mb-2">* campi obbligatori</div>
+            <form @submit.prevent="sendReview">
+                <!-- Input user name -->
+                <div class="mb-3">
+                    <label class="form-check-label me-2" for="name-review">Nome</label>
+                    <input type="text" name="name" id="name-review" v-model="userNameReview">
+                </div>
+                <div v-if="errors.name">
+                    <div v-for="error in errors.name" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div>
+                <!-- /Input user name -->
+
+                <!-- Input user description -->
+                <div class="mb-3">
+                    <label for="description-review" class="form-label ms-label">* Lascia la tua recensione:</label>
+                    <textarea name="description" rows="10" id="description-review" v-model="userMessageReview"></textarea>
+                </div>
+                <div v-if="errors.description">
+                    <div v-for="error in errors.description" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div>
+                <!-- Input user description -->
+
+                <!-- Accept TC -->
+                <!-- <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="accepted_tc" v-model="userTc">
+                    <label class="form-check-label" for="accepted_tc">Accetta termini e condizioni <router-link :to="{name: 'terms-and-conditions-reviews'}" target="_blank">Mostra termini e condizioni</router-link></label>
+                </div>
+                <div v-if="errors.accepted_tc">
+                    <div v-for="error in errors.accepted_tc" class="alert alert-danger" role="alert">
+                        {{ error }}
+                    </div>
+                </div> -->
+                <!-- /Accept TC -->
+
+                <button :disable="submitReview" type="submit" class="btn btn-brand">{{ submitReview ? 'Invio...' : 'Invia'}}</button>
+            </form>
+        </div>
+        <!-- /Form recensione dottore -->
+
     </section>
 </template>
 
@@ -73,6 +253,20 @@
             strong {
                 color: $primary-color;
             }
+        }
+        .ms-label{
+            display: block;
+        }
+        textarea{
+            width: 100%;
+        }
+        .contact,
+        .review-wrapper{
+            border: 2px solid white;
+            border-radius: 10px;
+        }
+        .danger{
+            color:red;
         }
     }
 </style>
