1086852223de0c6fc1d75fe8d0ff5ee9280405d4
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-07-04 10:10:59 +0200

    fix: if singlespecialisation

--- src/pages/SingleSpecialisation.vue
+++ src/pages/SingleSpecialisation.vue
@@ -115,21 +115,23 @@ export default {
                 <p style="color: red;">Nessun dottore trovato con questi criteri di ricerca</p>
             </div> -->
             <div v-for="doctor in filteredDoctors" class="doctors-wrapper card my-4 p-3" :key="doctor.user_slug">
-                <div class="card-wrapper d-flex align-items-center">
-                    <div class="img-wrapper me-3">
-                        <img v-if="doctor.photo" :src="`http://127.0.0.1:8000/storage/${doctor.photo}`" :alt="doctor.user_name">
-                    </div>
-                    <div class="info-wrapper">
-                        <p><strong>Nome</strong>: {{ doctor.user_name }}</p>
-                        <p><strong>Email</strong>: {{ doctor.user_mail }}</p>
-                        <p><strong>Specializzazione</strong>: {{ doctor.spec_name }}</p>
-                        <p><strong>Performance</strong>: {{ doctor.performance }}</p>
-                        <p><strong>Telefono</strong>: {{ doctor.telephone_number }}</p>
-                        <p v-for="review in reviews">
-                            <p v-if="review.profile_id === doctor.id"><strong>Rating</strong>: {{ review.average_score }}</p>
-                        </p>
-                        <p><strong>Numero recensioni</strong>: {{ reviewCount }}</p>
-                        <router-link :to="{ name: 'single-doctor', params: { slug: doctor.user_slug } }" class="btn btn-brand">Mostra dottore</router-link>
+                <div v-if="doctor.visibility">
+                    <div class="card-wrapper d-flex align-items-center">
+                        <div class="img-wrapper me-3">
+                            <img v-if="doctor.photo" :src="`http://127.0.0.1:8000/storage/${doctor.photo}`" :alt="doctor.user_name">
+                        </div>
+                        <div class="info-wrapper">
+                            <p><strong>Nome</strong>: {{ doctor.user_name }}</p>
+                            <p><strong>Email</strong>: {{ doctor.user_mail }}</p>
+                            <p><strong>Specializzazione</strong>: {{ doctor.spec_name }}</p>
+                            <p><strong>Performance</strong>: {{ doctor.performance }}</p>
+                            <p><strong>Telefono</strong>: {{ doctor.telephone_number }}</p>
+                            <p v-for="review in reviews">
+                                <p v-if="review.profile_id === doctor.id"><strong>Rating</strong>: {{ review.average_score }}</p>
+                            </p>
+                            <p><strong>Numero recensioni</strong>: {{ reviewCount }}</p>
+                            <router-link :to="{ name: 'single-doctor', params: { slug: doctor.user_slug } }" class="btn btn-brand">Mostra dottore</router-link>
+                        </div>
                     </div>
                 </div>
             </div>
